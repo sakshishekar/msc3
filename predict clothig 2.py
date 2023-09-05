ede2b6ad2ed8ee2ea5182386fb670f535db564b8
sakshishekar <142659388+sakshishekar@users.noreply.github.com>
2023-09-05 15:16:43 +0530

    Add files via upload

---
+++ predict clothig 2.py
@@ -0,0 +1,13 @@
+Python 2.7.16 (v2.7.16:413a49145e, Mar  4 2019, 01:37:19) [MSC v.1500 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====================== RESTART: C:/Python27/waitage.py ======================
+cannot predict clothing for 24 degrees.
+cannot predict clothing for 14 degrees.
+cannot predict clothing for 19 degrees.
+cannot predict clothing for 20 degrees.
+cannot predict clothing for 25 degrees.
+cannot predict clothing for 19 degrees.
+cannot predict clothing for 20 degrees.
+cannot predict clothing for 25 degrees.
+>>> 
