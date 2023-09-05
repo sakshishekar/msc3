283f9afd2cdd20158e5df4d3de42ea954a116de9
sakshishekar <142659388+sakshishekar@users.noreply.github.com>
2023-09-05 15:17:42 +0530

    Update predict clothig 2.py

--- predict clothig 2.py
+++ predict clothig 2.py
@@ -1,13 +1,47 @@
-Python 2.7.16 (v2.7.16:413a49145e, Mar  4 2019, 01:37:19) [MSC v.1500 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-====================== RESTART: C:/Python27/waitage.py ======================
-cannot predict clothing for 24 degrees.
-cannot predict clothing for 14 degrees.
-cannot predict clothing for 19 degrees.
-cannot predict clothing for 20 degrees.
-cannot predict clothing for 25 degrees.
-cannot predict clothing for 19 degrees.
-cannot predict clothing for 20 degrees.
-cannot predict clothing for 25 degrees.
->>> 
+class predictor:
+    def __init__(self):
+        self.minNo = 100
+        self.maxYes = 0
+        self.weight={}
+    def train(self, temp, wear):
+        if temp not in self.weight:
+            self.weight[temp] =0
+            
+        if self.maxYes>temp and wear == false :
+           self.maxYes=temp
+           self.weight[temp]= self.weight[temp] -1
+        if self.minNo<temp and wear == true :
+           self.minNo=temp
+           self.weight[temp]+=1
+                   
+    def test(self, temp):
+        if temp <= self.maxYes:
+            print("Recommended to wear warm clothes at {temp} degree.".format(temp=temp))
+            return True
+        elif temp >= self.minNo:
+            print("Not Recommended to wear warm clothes at {temp} degrees.".format(temp=temp))
+            return False
+        else:
+            print("cannot predict clothing for {temp} degrees.".format(temp=temp))
+            return 0
+                    
+p=predictor()
+p.train(24,False)
+p.train(14,True)
+p.train(19,True)
+p.train(20,True)
+p.train(25,False)
+p.train(19,True)
+p.train(20,True)
+p.train(25,False)
+
+p.test(24)
+p.test(14)
+p.test(19)
+p.test(20)
+p.test(25)
+p.test(19)
+p.test(20)
+p.test(25)
+
+
